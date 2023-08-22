72a9f239dec54371bc736f105114a5a2e0de499d
x47 <72315013+x47base@users.noreply.github.com>
2023-08-22 08:14:31 +0200

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -1,25 +1,13 @@
 const express = require('express');
-// var fs = require('fs');
-const route = express.Router();
+var fs = require('fs');
+const path = require('path');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.sendStatus(202);
+});
 
 const example = require('./example')
-route.use('/example', example)
+router.use('/example', example)
 
-// TODO: Fix TypeError: Router.use() requires a middleware function but got a Object
-
-/*
-route.use(express.static(__dirname));
-
-const fileList = fs.readdirSync(__dirname);
-
-for (const file of fileList) {
-  var mod = file.replace('.js','')
-  
-  if (mod == 'api') return null;
-  
-  console.log(file)
-  route.use(`/${file}`, require(`./${file}`))
-}
-*/
-
-module.exports = route;
+module.exports = router;
